6b5b5b25353a94c90f019af403f1cfbea0f26b9d
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-05-17 03:25:25 -0300

    data-mx-timestamp

--- src/util/sanitize.js
+++ src/util/sanitize.js
@@ -15,7 +15,7 @@ const urlSchemes = ['https', 'http', 'ftp', 'mailto', 'magnet', 'ipfs', 'bitcoin
 
 const permittedTagToAttributes = {
   font: ['style', 'data-mx-bg-color', 'data-mx-color', 'color'],
-  span: ['style', 'data-mx-bg-color', 'data-mx-color', 'data-mx-spoiler', 'data-mx-maths', 'data-mx-pill', 'data-mx-ping'],
+  span: ['style', 'data-mx-bg-color', 'data-mx-color', 'data-mx-timestamp', 'data-mx-spoiler', 'data-mx-maths', 'data-mx-pill', 'data-mx-ping'],
   div: ['data-mx-maths'],
   a: ['name', 'target', 'href', 'rel'],
   img: ['width', 'height', 'alt', 'title', 'src', 'data-mx-emoticon'],
