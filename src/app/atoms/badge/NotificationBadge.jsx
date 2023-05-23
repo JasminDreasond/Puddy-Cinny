9aa1072f897bca6324e290006e4dcdc4b0acb7e3
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-05-23 15:57:56 -0300

    NotificationBadge fix - bootstrap

--- src/app/atoms/badge/NotificationBadge.jsx
+++ src/app/atoms/badge/NotificationBadge.jsx
@@ -2,24 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './NotificationBadge.scss';
 
-function NotificationBadge({ alert, content, className }) {
-  const notificationClass = alert ? ' notification-badge--alert' : '';
-  const classes = ['badge', 'bg-light', 'notification-badge'];
+function NotificationBadge({ alert, content, className, bgColor }) {
+
+  const classes = ['badge', `bg-${bgColor}`, 'notification-badge'];
   if (className) classes.push(className);
+
+  let finalContent = '';
+  if (alert || content !== null) {
+    finalContent = content;
+  }
+
+  else {
+    const cicleClass = `fa-solid fa-circle text-${bgColor}`;
+    finalContent = (<i className={cicleClass} />);
+    classes.push('empty-badge');
+  }
+
   return (
-    <div className={`${classes.join(' ')}${notificationClass}`}>
-      {content !== null && content}
+    <div className={classes.join(' ')}>
+      {finalContent}
     </div>
   );
+
 }
 
 NotificationBadge.defaultProps = {
+  bgColor: 'light',
   alert: false,
   content: null,
   className: null,
 };
 
 NotificationBadge.propTypes = {
+  bgColor: PropTypes.string,
   alert: PropTypes.bool,
   className: PropTypes.string,
   content: PropTypes.oneOfType([
