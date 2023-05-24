7ad1dfe11feb5dc301663150a1e30a5e2c02ca6e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-05-24 14:21:13 -0300

    Button.scss file

--- src/app/atoms/button/Button.jsx
+++ src/app/atoms/button/Button.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Button.scss';
 
 import RawIcon from '../system-icons/RawIcon';
 import { blurOnBubbling } from './script';
