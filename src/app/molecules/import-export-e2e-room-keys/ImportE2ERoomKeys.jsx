aa6ec4a10151569e1cd375d21be7f590607a0340
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-05-25 14:03:19 -0300

    button fix

--- src/app/molecules/import-export-e2e-room-keys/ImportE2ERoomKeys.jsx
+++ src/app/molecules/import-export-e2e-room-keys/ImportE2ERoomKeys.jsx
@@ -112,9 +112,9 @@ function ImportE2ERoomKeys() {
             <Text>{keyFile.name}</Text>
           </div>
         )}
-        {keyFile === null && <Button onClick={() => inputRef.current.click()}>Import keys</Button>}
+        {keyFile === null && <Button className='me-3' onClick={() => inputRef.current.click()}>Import keys</Button>}
         <Input forwardRef={passwordRef} type="password" placeholder="Password" required />
-        <Button disabled={status.isOngoing} variant="primary" type="submit">Decrypt</Button>
+        <Button className='ms-3' disabled={status.isOngoing} variant="primary" type="submit">Decrypt</Button>
       </form>
       {status.type === cons.status.IN_FLIGHT && (
         <div className="import-e2e-room-keys__process">
