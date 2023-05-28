f91842597a3014f68e271261756d5895655ab2a8
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-05-28 01:12:17 -0300

    emoji - more test progress

--- src/app/organisms/emoji-board/EmojiBoardOpener.jsx
+++ src/app/organisms/emoji-board/EmojiBoardOpener.jsx
@@ -1,11 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import data from '@emoji-mart/data';
+import React, { useRef } from 'react';
 import Picker from '@emoji-mart/react';
-
-import cons from '../../../client/state/cons';
-import navigation from '../../../client/state/navigation';
-import settings from '../../../client/state/settings';
-import EmojiBoard from './EmojiBoard';
 
 function insertAtCursor(tinyField, myValue) {
 
@@ -14,13 +8,13 @@ function insertAtCursor(tinyField, myValue) {
   // IE support
   if (document.selection) {
     myField.focus();
-    let sel = document.selection.createRange();
+    const sel = document.selection.createRange();
     sel.text = myValue;
   }
   // MOZILLA and others
-  else if (myField.selectionStart || myField.selectionStart == '0') {
-    let startPos = myField.selectionStart;
-    let endPos = myField.selectionEnd;
+  else if (myField.selectionStart || myField.selectionStart === '0') {
+    const startPos = myField.selectionStart;
+    const endPos = myField.selectionEnd;
     myField.value = myField.value.substring(0, startPos)
       + myValue
       + myField.value.substring(endPos, myField.value.length);
@@ -32,53 +26,18 @@ function insertAtCursor(tinyField, myValue) {
 
 }
 
-let requestCallback = null;
-let isEmojiBoardVisible = false;
 function EmojiBoardOpener() {
+
   const openerRef = useRef(null);
-  const searchRef = useRef(null);
 
-  function openEmojiBoard(cords, requestEmojiCallback) {
-    if (requestCallback !== null || isEmojiBoardVisible) {
-      requestCallback = null;
-      if (cords.detail === 0) openerRef.current.click();
-      return;
-    }
+  /*
+  hexcode: null
+  mxc: "mxc://matrix.org/sICoLfcJSZCrxtlicYZsuJYJ"
+  shortcodes: ['pudding']
+  unicode: ":pudding:"
+  */
 
-    openerRef.current.style.transform = `translate(${cords.x}px, ${cords.y}px)`;
-    requestCallback = requestEmojiCallback;
-    openerRef.current.click();
-  }
-
-  function afterEmojiBoardToggle(isVisible) {
-    isEmojiBoardVisible = isVisible;
-    if (isVisible) {
-      if (!settings.isTouchScreenDevice) searchRef.current.focus();
-    } else {
-      setTimeout(() => {
-        if (!isEmojiBoardVisible) requestCallback = null;
-      }, 500);
-    }
-  }
-
-  function addEmoji(emoji) {
-    /*
-hexcode: null
-mxc: "mxc://matrix.org/sICoLfcJSZCrxtlicYZsuJYJ"
-shortcodes: ['pudding']
-unicode: ":pudding:"
-    */
-    requestCallback(emoji);
-  }
-
-  useEffect(() => {
-    navigation.on(cons.events.navigation.EMOJIBOARD_OPENED, openEmojiBoard);
-    return () => {
-      navigation.removeListener(cons.events.navigation.EMOJIBOARD_OPENED, openEmojiBoard);
-    };
-  }, []);
-
-  return <Picker data={data} onEmojiSelect={(emoji) => {
+  return <Picker set='twitter' onEmojiSelect={(emoji) => {
 
     const tinyData = {
       hexcode: emoji.unified.toUpperCase(),
@@ -93,7 +52,9 @@ unicode: ":pudding:"
     }
 
     // Insert Emoji
-    insertAtCursor(document.getElementById('message-textarea'), emoji.native);
+    const textarea = document.getElementById('message-textarea');
+    insertAtCursor(textarea, emoji.native);
+    textarea.focus();
 
   }} />;
 
