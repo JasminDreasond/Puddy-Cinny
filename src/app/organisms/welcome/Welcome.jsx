5effcf3ad3b82cade380620ba1d744e49db3c6b4
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-01 15:29:02 -0300

    welcome page

--- src/app/organisms/welcome/Welcome.jsx
+++ src/app/organisms/welcome/Welcome.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import './Welcome.scss';
 
 import Text from '../../atoms/text/Text';
 
 
 function Welcome() {
   return (
-    <div className="app-welcome flex--center">
-      <div>
+    <div className="p-3 border-0 rounded d-flex justify-content-center w-100 h-100 noselect" style={{ alignItems: 'center' }}>
+      <center>
         <img className="app-welcome__logo noselect" src="./public/favicon.ico" alt="Cinny logo" />
-        <Text className="app-welcome__heading" variant="h1" weight="medium" primary>
-          Welcome to Pony House
-        </Text>
-        <Text className="app-welcome__subheading" variant="s1">
-          The tiny Pony House matrix client
-        </Text>
-      </div>
+
+        <h2>Welcome to Pony House</h2>
+
+        <h6>The tiny Pony House matrix client</h6>
+
+      </center>
     </div>
   );
 }
