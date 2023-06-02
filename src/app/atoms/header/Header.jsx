0834bf17b52554e6b2a9be1bb7753249f94cdcc6
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-01 23:38:23 -0300

    preparing new banner header

--- src/app/atoms/header/Header.jsx
+++ src/app/atoms/header/Header.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Header({ children, title }) {
   return (
-    <nav id="space-header" className="navbar navbar-expand navbar-nav p-0 w-100 d-block noselect border-bottom border-bg emoji-size-fix">
+    <nav className="navbar navbar-expand navbar-nav p-0 w-100 d-block noselect border-bottom border-bg emoji-size-fix">
       <div className='container-fluid'>
         {(typeof title === 'string' && <span className="navbar-brand">{title}</span>)}
         <div className="navbar-collapse py-1 px-2" >
