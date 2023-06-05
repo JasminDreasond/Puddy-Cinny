44d5e0212174b26de705da0412dc9a7c83c7f9a7
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-05 18:47:32 -0300

    pwa

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,11 @@ import './default.scss';
 import './index.scss';
 
 import settings from './client/state/settings';
+import { getPWADisplayMode } from '../src/util/PWA.js'
 
 import App from './app/pages/App';
 
+getPWADisplayMode();
 settings.applyTheme();
 
 ReactDom.render(<App />, document.getElementById('root'));
