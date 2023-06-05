6487489ae1869ab82bed74c1943a2d81a91df12b
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-05 02:08:23 -0300

    temp

--- src/app/atoms/avatar/Avatar.jsx
+++ src/app/atoms/avatar/Avatar.jsx
@@ -12,64 +12,6 @@ import RawIcon from '../system-icons/RawIcon';
 import ImageBrokenSVG from '../../../../public/res/svg/image-broken.svg';
 import { avatarInitials } from '../../../util/common';
 
-function freezeGif(img, wantedWidth) {
-
-  function createElement(type, callback) {
-    const element = document.createElement(type);
-
-    callback(element);
-
-    return element;
-  }
-
-  const { width } = img;
-  const { height } = img;
-
-  const canvas = createElement('canvas', clone => {
-    clone.width = width;
-    clone.height = height;
-  });
-  let attr;
-  let i = 0;
-
-  const freeze = () => {
-
-    const ctx = canvas.getContext('2d');
-
-    if (wantedWidth) {
-      const aspect = width / height;
-      canvas.width = wantedWidth;
-      canvas.height = wantedWidth / aspect;
-    } else {
-      canvas.width = width;
-      canvas.height = height;
-    }
-
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-    for (i = 0; i < img.attributes.length; i++) {
-      attr = img.attributes[i];
-
-      if (attr.name !== '"') { // test for invalid attributes
-        canvas.setAttribute(attr.name, attr.value);
-      }
-    }
-
-    canvas.classList.add('normal-avatar');
-    canvas.classList.remove('anim-avatar');
-
-    img.parentNode.insertBefore(canvas, img);
-
-  };
-
-  if (img.complete) {
-    freeze();
-  } else {
-    img.addEventListener('load', freeze, true);
-  }
-
-};
-
 const Avatar = React.forwardRef(({
   text, bgColor, iconSrc, faSrc, iconColor, imageSrc, size, className, imgClass, imageAnimSrc
 }, ref) => {
@@ -96,24 +38,14 @@ const Avatar = React.forwardRef(({
 
             :
 
-            <>
-              <img
-                className={`anim-avatar ${imgClass}`}
-                draggable="false"
-                src={imageAnimSrc}
-                onLoad={(e) => { e.target.style.backgroundColor = 'transparent'; /*freezeGif(e.target, 42);*/ }}
-                onError={(e) => { e.target.src = ImageBrokenSVG; }}
-                alt=""
-              />
-              <img
-                className={`normal-avatar ${imgClass}`}
-                draggable="false"
-                src={imageSrc}
-                onLoad={(e) => { e.target.style.backgroundColor = 'transparent'; }}
-                onError={(e) => { e.target.src = ImageBrokenSVG; }}
-                alt=""
-              />
-            </>
+            <img
+              className={`anim-avatar ${imgClass}`}
+              draggable="false"
+              src={imageSrc}
+              onLoad={(e) => { e.target.style.backgroundColor = 'transparent'; }}
+              onError={(e) => { e.target.src = ImageBrokenSVG; }}
+              alt=""
+            />
 
           )
           : faSrc !== null
