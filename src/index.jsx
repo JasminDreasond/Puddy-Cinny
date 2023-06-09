dae276287dbaedac807bdd56801f9b68ccfda76b
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-06-08 22:55:30 -0300

    temp

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import 'highlight.js/styles/github.css';
 import './default.scss';
 import './index.scss';
 
-import { startWeb3 } from './util/web3';
+// import { startWeb3 } from './util/web3';
 
 import settings from './client/state/settings';
 import { getPWADisplayMode } from '../src/util/PWA.js'
@@ -13,6 +13,6 @@ import App from './app/pages/App';
 
 getPWADisplayMode();
 settings.applyTheme();
-startWeb3();
+// startWeb3();
 
 ReactDom.render(<App />, document.getElementById('root'));
